a8e735ed3b2638d71f014ba38ae9025ac0d92547
Quadref <quadref.works@gmail.com>
2020-09-09 21:50:01 +0300

    Initial Commit

---
+++ strategy_design_patterns/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  strategy_design_patterns
+//
+//  Created by Quadref on 09/09/2020.
+//  Copyright © 2020 Quadref. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
