bba74c2992983894a991e046966a665bec910178
Quadref <quadref.works@gmail.com>
2020-09-10 00:03:38 +0300

    Initial commit

--- strategy_design_patterns/main.swift
+++ strategy_design_patterns/main.swift
@@ -8,5 +8,67 @@
 
 import Foundation
 
-print("Hello, World!")
+var str = readLine()
 
+print("Input: ")
+
+protocol BasicStrategy {
+    func inputNumber()
+    func inputLowerCase(input: String) -> Bool
+    func inputUppercase(input: String) -> Bool
+}
+
+class IStrategy: BasicStrategy {
+    func inputNumber() {
+        let possibleInt = str ?? ""
+
+        if let convertedNumber = Int(possibleInt) {
+            print("\(possibleInt) is an Int")
+        }
+        
+    }
+    
+    func inputLowerCase(input: String) -> Bool {
+        for str in input {
+           if (!(str >= "a" && str <= "z") && !(str >= "а" && str <= "я") ) {
+            return false
+           }
+        }
+        print("\(str) is an lower case")
+        return true
+    }
+    
+    func inputUppercase(input: String) -> Bool {
+        for str in input {
+           if (!(str >= "A" && str <= "Z") && !(str >= "А" && str <= "Я") ) {
+            return false
+           }
+        }
+        print("\(str) is an uppercase")
+        return true
+    }
+    
+}
+
+
+class Main {
+    var strategy: BasicStrategy
+    
+    init(strategy: BasicStrategy) {
+        self.strategy = strategy
+    }
+    
+    func makeAction() {
+        self.strategy.inputNumber()
+        self.strategy.inputLowerCase(input: str ?? "")
+        self.strategy.inputUppercase(input: str ?? "")
+    }
+    
+    func changeStrategy(aStrategy: BasicStrategy) {
+        self.strategy = aStrategy
+    }
+}
+
+let a = IStrategy()
+let p = Main(strategy: a)
+p.makeAction()
